b7b9ee69861bba52614cf230293c64f9b5b837ee
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-03-03 21:06:44 -0300

    1.1-Instituicoes.py

---
+++ 1.1-Intituicoes.py
@@ -0,0 +1,43 @@
+'''
+Mundo
+    Sociedade-Institucionalizadas
+        Organizações
+            Produções
+                Bens(produtos)
+                Serviçoes
+                    Planejadas
+                    Coordenadas
+                    Dirigidas
+                        Recursos
+                            Humanos (Pessoas)
+                            Não Humanos
+                                Físicos
+                                Materias
+                                Financeiros
+                                Tecnológicos
+
+'''
+
+'''
+    A Vida das pessoas depende intimamente das Organizações
+    E as Organizações dependem das pessoas
+'''
+
+'''
+    As Organizações são extremamente heterogêneas e diversificadas
+        Tamanhos
+        Características
+        Estruturas
+        Objetivos
+            Finalidades
+                Lucrativas (Empresas)
+                Não Lucrativas
+                    Exército
+                    Igreja
+                    Serviçoes Públicos
+                    Entidades Filantrópicas
+                    ONGS - Não Governamentais
+'''
+
+TO = 'Teoria Geral das Organizações'
+#pg.30/650
